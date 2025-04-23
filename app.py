5905cbc9c95a779a882441217ef5abebae6d62b2
Allyson Vinicius <98901653+Allhons@users.noreply.github.com>
2025-04-22 22:57:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import re
 
 
 # Windows: Descomente e configure se necessário C:\Program Files\Tesseract-OCR
-pytesseract.pytesseract.tesseract_cmd = "apt-packages.txt"
+# pytesseract.pytesseract.tesseract_cmd = "apt-packages.txt"
 
 st.title("🧠 Extrator de textos")
 st.write("Faça upload de ou ou mais imagens, e o sistema extrai colunas (em MAIÚSCULAS com `:`) e organiza os dados em um DataFrame único.")
